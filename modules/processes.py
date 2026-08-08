b4c16e6dc51ba4e88f4d0e5aea6a9d8efcf58193
Ayush Verma <ayushverma.codes@gmail.com>
2026-08-08 08:31:49 +0530

    Filtered ps process from top CPU list

--- modules/processes.py
+++ modules/processes.py
@@ -2,18 +2,21 @@ import subprocess
 
 def get_top_processes(limit=5):
     result = subprocess.check_output(
-        ["ps", "-eo", "pid,comm,%cpu", "--sort=-%cpu"],
+        "ps -eo pid,comm,%cpu --sort=-%cpu | head -n 6",
+        shell=True,
         text=True
     )
 
-    lines = result.strip().split("\n")[1:limit+1]
+    lines = result.strip().split("\n")[1:limit + 1]
 
     processes = []
 
     for line in lines:
         parts = line.split(None, 2)
+
         if len(parts) == 3:
             pid, name, cpu = parts
+
             processes.append({
                 "pid": pid,
                 "name": name,
